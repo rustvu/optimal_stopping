2b21d957ff26d9dab063362f3b1c3ead46de12a0
Peter Volgyesi <peter.volgyesi@gmail.com>
2026-09-07 10:06:53 -0500

    better plotting, removing dynamic parts for easier understanding

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@
 use fastrand;
 
 const N_SUITORS: usize = 100;
+const N_EXPERIMENTS: usize = 100_000;
 
 /// Suitors are represented as random integer numbers (full range)
 /// where a higher numbers mean more desirable suitor
 fn generate_suitors() -> [i32; N_SUITORS] {
     let mut suitors = [0; N_SUITORS];
-    for i in 0..suitors.len() {
+    for i in 0..N_SUITORS {
         suitors[i] = fastrand::i32(..);
     }
     suitors
@@ -18,11 +19,10 @@ fn generate_suitors() -> [i32; N_SUITORS] {
 /// Pick a prince (best suitor) using a simple exploration/exploitation strategy
 /// The `n_explore` parameter controls the exploration size
 fn pick_a_prince(suitors: [i32; N_SUITORS], n_explore: usize) -> i32 {
-    assert!(suitors.len() > 0);
-    assert!(suitors.len() >= n_explore);
+    assert!(n_explore <= N_SUITORS);
 
     let mut bar = i32::MIN;
-    for i in 0..suitors.len() {
+    for i in 0..N_SUITORS {
         if i < n_explore {
             bar = if suitors[i] > bar { suitors[i] } else { bar };
         } else {
@@ -32,17 +32,16 @@ fn pick_a_prince(suitors: [i32; N_SUITORS], n_explore: usize) -> i32 {
         }
     }
 
-    suitors[suitors.len() - 1]
+    suitors[N_SUITORS - 1]
 }
 
 fn main() {
     let mut optimum = (0, 0); // (score, explore)
-    let mut scores = vec![];
+    let mut scores = [0; N_SUITORS];
 
     for n_explore in 0..N_SUITORS {
-        let n_experiments = 100_000;
         let mut score = 0;
-        for _ in 0..n_experiments {
+        for _ in 0..N_EXPERIMENTS {
             let suitors = generate_suitors();
             let best_suitor = *suitors.iter().max().unwrap();
             let prince = pick_a_prince(suitors, n_explore);
@@ -52,7 +51,7 @@ fn main() {
                 score += 1;
             }
         }
-        scores.push(score);
+        scores[n_explore] = score;
         if score > optimum.0 {
             optimum = (score, n_explore);
         }
@@ -63,18 +62,38 @@ fn main() {
     // Optional Plotting
     #[cfg(feature = "plotly")]
     {
-        use plotly::{layout::Axis, Layout, Plot, Scatter};
+        use plotly::{
+            common::Mode,
+            layout::{Axis, Shape, ShapeLine, ShapeType},
+            Layout, Plot, Scatter,
+        };
 
         let mut plot = Plot::new();
-        let trace = Scatter::new((0..scores.len()).collect(), scores);
+        let trace = Scatter::new((0..scores.len()).collect(), Vec::from(scores));
         plot.add_trace(trace);
+
+        let optimum_marker = Scatter::new(vec![optimum.1], vec![optimum.0]).mode(Mode::Markers);
+        plot.add_trace(optimum_marker);
 
         let layout = Layout::new()
             //.width(800)
             .height(600)
+            .show_legend(false)
             .title("Optimal Stopping")
-            .x_axis(Axis::new().title("n_explore"))
-            .y_axis(Axis::new().title("score"));
+            .x_axis(
+                Axis::new()
+                    .title("n_explore")
+                    .tick_values(vec![optimum.1 as f64])
+                    .tick_text(vec![optimum.1.to_string()]),
+            )
+            .y_axis(Axis::new().title("score"))
+            .shapes(vec![Shape::new()
+                .shape_type(ShapeType::Line)
+                .x0(optimum.1 as f64)
+                .x1(optimum.1 as f64)
+                .y0(0.0)
+                .y1(optimum.0 as f64)
+                .line(ShapeLine::new().color("red"))]);
         plot.set_layout(layout);
         plot.show();
     }
